daef0393982dd4fdb8240b120f8abe86402f3c40
ichx123 <ichx123@icloud.com>
2024-11-28 09:26:05 +0100

    feat: add function to draw and erase the drawing
    With Eventlisteners when the user hovers over the squares, they can
    draw black squares. If they click the button "Mülleimer", the grid
    will be empty again. It will assign a white background-color to the squares.

--- javascript.js
+++ javascript.js
@@ -2,9 +2,36 @@
 const container = document.querySelector(".container");
 const clone = container.cloneNode(true);
 
+// Creating a default Grid
+createGrid(16);
+
+//Variables to get the divs for coloring them
+const squares = document.querySelectorAll(".square");
+
+//EventListener for Hovering the Squares
+squares.forEach((square) => {
+    square.addEventListener("mouseenter", () => {
+        square.style.backgroundColor = "black";
+    });
+});
+
+//Variables for Buttons
+const eraseDrawingButton = document.querySelector("#eraser");
+const newGridButton = document.querySelector("#new-grid");
+
+//Eventlistener for Buttons 
+eraseDrawingButton.addEventListener("click", eraseDrawing);
+newGridButton.addEventListener("click", () => {
+   let numOfSquares = parseInt(prompt("How many squares would you like?", 16));
+   clearContainer();
+   createGrid(numOfSquares);
+})
+
 //Function to create a grid of suqares for the sketchpad
 function createGrid(numOfSquares) {
 
+    const container = document.querySelector(".container");
+    
     for (let i = 1; i <= numOfSquares; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
@@ -24,8 +51,12 @@ function createGrid(numOfSquares) {
 
 //Function to empty the container before populating it again
 function clearContainer() {
-    container.replaceWith(clone);
+    container = container.replaceWith(clone);
 }
 
-
-createGrid(16);
+//Function, to empty the grid to start drawing again on the same grid
+function eraseDrawing() {
+    squares.forEach((square) => {
+        square.style.backgroundColor = "white";
+    });
+}
