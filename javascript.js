06fe86e2d66294f437d7f5eac6e4c0d7b8f16b25
ichx123 <ichx123@icloud.com>
2024-11-28 09:52:42 +0100

    fix: createGrid working after pressing "New Grid"
    The function wasn't working, because of "replaceWith", in DOM it will
    be looking the same, but we can't access the container-variable after
    replacing it.
    
    Fix is to use replaceChildren to empty the container before populating
    it again.
    
    Also the eventListeners wasn't working either after generating a new grid,
    because the querySelector was initialized before with the old squares.
    To prevent this, the EventListeners was refactored into a function and
    called inside the "createGrid"-function to renew them each time a new
    grid is generated.

--- javascript.js
+++ javascript.js
@@ -1,19 +1,5 @@
-// Variables to store Container and it's empty Clone for setbacks
-const container = document.querySelector(".container");
-const clone = container.cloneNode(true);
-
 // Creating a default Grid
 createGrid(16);
-
-//Variables to get the divs for coloring them
-const squares = document.querySelectorAll(".square");
-
-//EventListener for Hovering the Squares
-squares.forEach((square) => {
-    square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "black";
-    });
-});
 
 //Variables for Buttons
 const eraseDrawingButton = document.querySelector("#eraser");
@@ -22,16 +8,21 @@ const newGridButton = document.querySelector("#new-grid");
 //Eventlistener for Buttons 
 eraseDrawingButton.addEventListener("click", eraseDrawing);
 newGridButton.addEventListener("click", () => {
-   let numOfSquares = parseInt(prompt("How many squares would you like?", 16));
-   clearContainer();
-   createGrid(numOfSquares);
-})
+    let numOfSquares = parseInt(prompt("How many squares would you like? Put in a number between 1 and 100.", 16));
+    if ((numOfSquares <= 100) && (numOfSquares > 10)) {
+        createGrid(numOfSquares);
+    } else {
+        alert("Error, you put in a invalid number. Please put in a number between 1 and 100.");
+        numOfSquares = parseInt(prompt("How many squares would you like? Put in a number between 1 and 100.", 16));
+    }
+});
 
 //Function to create a grid of suqares for the sketchpad
 function createGrid(numOfSquares) {
 
     const container = document.querySelector(".container");
-    
+    clearContainer();
+
     for (let i = 1; i <= numOfSquares; i++) {
         const row = document.createElement("div");
         row.classList.add("row");
@@ -39,24 +30,40 @@ function createGrid(numOfSquares) {
         createSquares(row, numOfSquares);
     }
 
+    addListenersToSquares();
 
+    //Function to create the squares for the rows
     function createSquares(forDiv, num) {
         for (let i = 1; i <= num; i++) {
             const div = document.createElement("div");
             div.classList.add("square");
             forDiv.appendChild(div);
         }
-    } //end function createSquares
-} // end function createGrid
+    }
 
-//Function to empty the container before populating it again
-function clearContainer() {
-    container = container.replaceWith(clone);
-}
+    //Function to empty the container before populating it again
+    function clearContainer() {
+        container.replaceChildren();
+    }
+
+} // end function createGrid
 
 //Function, to empty the grid to start drawing again on the same grid
 function eraseDrawing() {
+    const squares = document.querySelectorAll(".square");
     squares.forEach((square) => {
         square.style.backgroundColor = "white";
+    });
+}
+
+function addListenersToSquares() {
+    //Variable to get the divs for coloring them
+    const squares = document.querySelectorAll(".square");
+
+    //EventListener for Hovering the Squares
+    squares.forEach((square) => {
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = "black";
+        });
     });
 }
